04486c61926dcdac010e79cebe628696c710c558
Ewa Wojakowska <aniaamuz@gmail.com>
2025-12-13 09:34:41 +0100

    dodanie nowego pliku

---
+++ zad_1.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self, name: str, marks: list[float]):
+        self.name = name
+        self.marks = marks
+    def is_passed(self) -> bool:
+        average = sum(self.marks) / len(self.marks)
+        return average > 50
+
+student1 = student("Anna", [70,65,80])
+student2 = student("Daniel", [40,20,35])
+
+print(student1.is_passed())
+print(student2.is_passed())
