8bdbaa80814149e44558e8c6bb5fc6bd0b59892d
Ewa Wojakowska <aniaamuz@gmail.com>
2025-12-13 13:27:32 +0100

    pep 8

--- zad_3.py
+++ zad_3.py
@@ -4,20 +4,31 @@ class Property:
         self.rooms = rooms
         self.price = price
         self.address = address
+
     def __str__(self):
-        return f'Property has {self.area} area, {self.rooms} rooms and costs {self.price} dollars.'
+        return (
+            f'Property has {self.area} area, {self.rooms} rooms '
+            f'and costs {self.price} dollars.'
+        )
+
+
 class House(Property):
-    def __init__(self, area, rooms, price, address,plot: int):
+    def __init__(self, area, rooms, price, address, plot: int):
         super().__init__(area, rooms, price, address)
         self.plot = plot
+
     def __str__(self):
         return f'House has {self.plot} square meters'
+
+
 class Flat(Property):
     def __init__(self, area, rooms, price, address, floor: str):
         super().__init__(area, rooms, price, address)
         self.floor = floor
+
     def __str__(self):
         return f'Flat is located on the {self.floor} floor'
+
 
 house = House(120, 5, 350000, "ul. Bytomska 12, Warszawa", 500)
 flat = Flat(60, 3, 220000, "ul. Warszawska 21, Bytom", 3)
